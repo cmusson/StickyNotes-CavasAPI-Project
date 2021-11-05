b3b030010987c572ef177b48107ab64a0791674a
Connor <mussonconnor@gmail.com>
2021-11-05 16:04:40 +0100

    app without canvas feature

--- client-sticky-notes/src/App.js
+++ client-sticky-notes/src/App.js
@@ -57,7 +57,19 @@ function App() {
 
   // deletes the note via filter method
   const deleteNote = (e) => {
-    setNotes(notes.filter((note) => note.id !== e.currentTarget.id));
+    let updatedNotes = notes.filter((note) => note.id !== e.currentTarget.id);
+    setNotes([...updatedNotes]);
+  };
+
+  // changes color of the note when selected
+  const changeColor = (e) => {
+    let updatedNotes = notes;
+    updatedNotes.forEach((el) => {
+      if (el.id === e.target.parentNode.id) {
+        el.color = e.target.id;
+      }
+    });
+    setNotes([...updatedNotes]);
   };
 
   return (
@@ -106,14 +118,30 @@ function App() {
               <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.538l-4.592-4.548 4.546-4.587-1.416-1.403-4.545 4.589-4.588-4.543-1.405 1.405 4.593 4.552-4.547 4.592 1.405 1.405 4.555-4.596 4.591 4.55 1.403-1.416z" />
             </svg>
           </div>
-          <form className="note-form">
-            <textarea
-              id={stickyNote.id}
-              className="note-body"
-              value={stickyNote.text}
-              onChange={editNote}
-            ></textarea>
-          </form>
+          <div className="form-colors">
+            <form className="note-form">
+              <textarea
+                id={stickyNote.id}
+                className="note-body"
+                value={stickyNote.text}
+                onChange={editNote}
+              ></textarea>
+            </form>
+            <div className="pallet" id={stickyNote.id}>
+              <div className="white color" id={"white"} onClick={changeColor} />
+              <div className="red color" id={"#ef476f"} onClick={changeColor} />
+              <div
+                className="blue color"
+                id={"#007aff"}
+                onClick={changeColor}
+              />
+              <div
+                className="yellow color"
+                id={"#ffc145"}
+                onClick={changeColor}
+              />
+            </div>
+          </div>
         </div>
       ))}
     </div>
